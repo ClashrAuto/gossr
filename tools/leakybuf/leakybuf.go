ab67d2bccea041a9af066e20594420d6bdef1e75
zu1k <lvguangfa@gmail.com>
2019-09-14 16:20:25 +0800

    mod buf size to 32*1024

--- tools/leakybuf/leakybuf.go
+++ tools/leakybuf/leakybuf.go
@@ -40,8 +40,8 @@ func (lb *LeakyBuf) Put(b []byte) {
 }
 
 const (
-	GlobalLeakyBufSize = 32 * 1024 // data.len(2) + hmacsha1(10) + data(4096)
-	maxNBuf            = 8192
+	GlobalLeakyBufSize = 32 * 1024 // data.len(2) + hmacsha1(10) + data(40960)
+	maxNBuf            = 32 * 1024
 )
 
 var GlobalLeakyBuf = NewLeakyBuf(maxNBuf, GlobalLeakyBufSize)
